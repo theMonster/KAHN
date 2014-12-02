cf36cae20531691e55a376cec27065051ed0d54c
theMonster <calebjj@me.com>
2014-12-01 21:57:05 -0700

    Added ServiceObjects and service

---
+++ Kahn/Kahn/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  Kahn
+//
+//  Created by Caleb Jonassaint on 12/1/14.
+//  Copyright (c) 2014 Caleb Jonassaint. All rights reserved.
+//
+
+import Foundation
+
+class ServiceObject {
+    
+}
+
+class Service {
+    
+}
