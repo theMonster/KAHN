9fa65837df9c5be9fc880697544f39d38d977a73
theMonster <calebjj@me.com>
2014-12-01 21:45:17 -0700

    Initial commit

---
+++ Kahn/Kahn/Kahn.h
@@ -0,0 +1,19 @@
+//
+//  Kahn.h
+//  Kahn
+//
+//  Created by Caleb Jonassaint on 12/1/14.
+//  Copyright (c) 2014 Caleb Jonassaint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kahn.
+FOUNDATION_EXPORT double KahnVersionNumber;
+
+//! Project version string for Kahn.
+FOUNDATION_EXPORT const unsigned char KahnVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kahn/PublicHeader.h>
+
+
